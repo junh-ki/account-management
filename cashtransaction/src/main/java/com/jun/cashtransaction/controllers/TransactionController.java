b906aee8b573d9f53ecf5f7203ba2c6f6a2df1f7
junh-ki <kijoonh91@gmail.com>
2021-04-23 11:17:11 +0200

    Transaction Management Annotation added to 'completeTransaction'
    Change-Id: I97acb96f8a2ff6861e99bf6c1a99c89d1f73f705
    Signed-off-by: junh-ki <kijoonh91@gmail.com>

--- cashtransaction/src/main/java/com/jun/cashtransaction/controllers/TransactionController.java
+++ cashtransaction/src/main/java/com/jun/cashtransaction/controllers/TransactionController.java
@@ -2,6 +2,7 @@ package com.jun.cashtransaction.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,6 +34,7 @@ public class TransactionController {
 	}
 	
 	@RequestMapping("/completeTransaction")
+	@Transactional
 	public String completeTransaction(@RequestParam("senderAccountID") Long senderAccountID, @RequestParam("recipientAccountID") Long recipientAccountID,
 			@RequestParam("amount") Double amount, ModelMap modelMap) {
 		Account senderAccount = accountRestClient.findAccountById(senderAccountID);
